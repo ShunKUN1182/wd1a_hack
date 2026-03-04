4f627c7ace4b49098accca8a61532ed3f3e5ff55
ShunKUN1182 <2250378@ecc.ac.jp>
2026-03-04 11:10:04 +0900

    feat: imgをlocal storageに保存

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
 const btns = document.querySelectorAll(".btn");
 const modal = document.querySelector(".modal");
 const backBtn = document.querySelector("#backBtn");
+const radioIcon = document.querySelectorAll("label");
+
+console.log(radioIcon);
 
 console.log(btns);
 
 btns.forEach((e) => {
     e.addEventListener("click", (event) => {
-        // console.log("foo");
         event.preventDefault();
         modal.style.display = "flex";
     });
@@ -14,4 +16,11 @@ btns.forEach((e) => {
 
 backBtn.addEventListener("click", () => {
     modal.style.display = "none";
+});
+
+radioIcon.forEach((e) => {
+    e.addEventListener("click", () => {
+        const iconImgPass = e.querySelector("img").getAttribute("src");
+        localStorage.setItem("imgPass", iconImgPass);
+    });
 });
